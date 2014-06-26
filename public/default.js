82117ea44cdf79f0ce0f72e064579e129bec415c
Harris Miller <harrismiller08@gmail.com>
2014-06-25 23:24:24 -0600

    initial commit

---
+++ public/default.js
@@ -0,0 +1,41 @@
+
+var socket;
+
+$(function() {
+	socket = io.connect('http://localhost:8080');
+	socket.on('connect', addUser);
+	socket.on('updatechat', processMessage);
+	socket.on('updateusers', updateUserList);
+	$('#datasend').on('click', sendMessage);
+	$('#data').on('keypress', processEnterPress);
+});
+
+function addUser() {
+	socket.emit('adduser', prompt('What\'s your name?'));
+};
+
+function processMessage(username, data) {
+	$('<b>' + username + ':<b> ' + data + '<br>').insertAfter($('#conversation'));
+};
+
+function updateUserList(data) {
+	$('#users').empty();
+	$.each(data, function(key, value) {
+		$('#users').append('<div>' + key + '</div>');
+	});
+};
+
+function sendMessage() {
+	var message = $('#data').val();
+	$('#data').val('');
+	socket.emit('sendchat', message);
+	$('#data').focus();
+};
+
+function processEnterPress(e) {
+	if (e.which == 13) {// enter
+		e.preventDefault();
+		$(this).blur();
+		sendMessage();
+	}
+};
